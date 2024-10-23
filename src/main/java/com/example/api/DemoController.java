b67df2227cd49e78789310124f8d9dba783e6813
Denis Jajčević <djajcevic@croz.net>
2024-10-23 07:32:54 +0200

    demo credential leak

--- src/main/java/com/example/api/DemoController.java
+++ src/main/java/com/example/api/DemoController.java
@@ -3,6 +3,8 @@ package com.example.api;
 import com.example.annotation.SuppressFBWarnings;
 import com.example.dbo.User;
 import com.example.repo.UserRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -20,6 +22,9 @@ import java.util.List;
 @SuppressFBWarnings("SPRING_ENDPOINT")
 public class DemoController {
 
+
+    private static Logger LOG = LoggerFactory.getLogger(DemoController.class);
+
     private final UserRepo userRepo;
 
     @Autowired
@@ -34,10 +39,10 @@ public class DemoController {
         return userRepo.findAll();
     }
 
-    @GetMapping("/sqlInjection")
-    public String hello(@RequestParam String name) {
-        String sql = "SELECT message FROM greetings WHERE name = '" + name + "'";
-        return jdbcTemplate.queryForObject(sql, String.class);
+    @GetMapping("/unsecure")
+    public String hello(@RequestParam String username, @RequestParam String password) {
+        LOG.info("password " + password);
+        return "Hello " + username + " " + password;
     }
 
 }
